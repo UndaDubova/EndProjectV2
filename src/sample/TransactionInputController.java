b4531cc53a3cb1cdc3322fa9e2d85c913c30375f
UndaDubova <undadubova@gmail.com>
2022-02-26 11:54:14 +0200

    Database connection + JavaFX first try

--- src/sample/TransactionInputController.java
+++ src/sample/TransactionInputController.java
@@ -5,20 +5,24 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
+import javafx.util.converter.LocalDateStringConverter;
 
 import java.io.File;
 import java.net.URL;
 import java.sql.Connection;
+import java.time.*;
 import java.util.ResourceBundle;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class TransactionInputController implements Initializable {
-    @FXML private TextField DateInputBox;
-    @FXML private TextField PurchaseInputBox;
-    @FXML private TextField PriceInputBox;
-    @FXML private Button BrowseForFile;
+    @FXML private DatePicker dateInput;
+    @FXML private TextField purchaseInput;
+    @FXML private TextField priceInput;
+    @FXML private ChoiceBox currencyInput;
+    String transactionInputFilePath;
+
 
     DatabaseManager databaseManager;
     Connection conn;
@@ -42,25 +46,16 @@ public class TransactionInputController implements Initializable {
        }
 
     }
-    @FXML
-    private void AddDate(ActionEvent event){
-        System.out.println(DateInputBox.getText());
-        String Date = DateInputBox.getText();
-    }
-    @FXML
-    private void AddPurchase(ActionEvent event){
-        System.out.println(PurchaseInputBox.getText());
-        String Purchase = PurchaseInputBox.getText();
-    }
-    @FXML
-    private void AddPrice(ActionEvent event){
-        System.out.println(PriceInputBox.getText());
-        double Price = Double.parseDouble(PriceInputBox.getText());
-    }
+
     @FXML
     private void AddButtonPressed(ActionEvent event){
-        System.out.println("Date + Purchase + Price");
+        LocalDate date = dateInput.getValue();
+        String purchase = purchaseInput.getText();
+        double price = Double.parseDouble(priceInput.getText());
+        System.out.println(date + purchase + price);
     }
+
+    //@FXML private void
 }
 
 
